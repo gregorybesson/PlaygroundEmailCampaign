6b43ee47b60f8cca0ca6e61bdeee3639af0921b4
stephaniefaureadfab <stephanie.faure@adfab.fr>
2013-12-10 11:58:27 +0100

    initial commit

---
+++ src/PlaygroundEmailCampaign/Module.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace PlaygroundEmailCampaign;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\Validator\AbstractValidator;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $sm = $e->getApplication()->getServiceManager();
+
+        $options = $sm->get('playgroundcore_module_options');
+        $locale = $options->getLocale();
+        $translator = $sm->get('translator');
+        if (!empty($locale)) {
+            //translator
+            $translator->setLocale($locale);
+
+            // plugins
+            $translate = $sm->get('viewhelpermanager')->get('translate');
+            $translate->getTranslator()->setLocale($locale);
+        }
+        AbstractValidator::setDefaultTranslator($translator,'playgroundcore');
+
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+
+        // Here we need to schedule the core cron service
+
+        // If cron is called, the $e->getRequest()->getPost() produces an error so I protect it with
+        // this test
+        if ((get_class($e->getRequest()) == 'Zend\Console\Request')) {
+            return;
+        }
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoLoader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__.'/../../src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../../config/module.config.php';
+    }
+
+
+    public function getServiceConfig()
+    {
+        return array(
+            'aliases' => array(
+            ),
+
+            'invokables' => array(
+
+            ),
+
+            'factories' => array(
+            ),
+        );
+    }
+}
